4cf33aaf2fee775fb4931bea77fbc880e765779d
tarantula pi <peneetaannwojcik@gmail.com>
2026-03-30 14:19:46 +0000

    ugh

--- update_ino.sh
+++ update_ino.sh
@@ -1,5 +1,5 @@
 PORT=/dev/ttyACM0
-CODE=/testing/arduino_xbox/
+CODE=./testing/arduino_xbox/
 
 echo "Compiling and uploading to ${PORT}..."
 arduino-cli compile --fqbn arduino:avr:uno ${CODE} --upload -p ${PORT} && echo "Done!"
