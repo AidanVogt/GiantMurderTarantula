7783d03d8dd3301f77ba2f90c62e572fe7e1df4a
Peneeta Wojcik <94078632+peneeta@users.noreply.github.com>
2026-03-30 10:17:49 -0400

    updateino

--- update_ino.sh
+++ update_ino.sh
@@ -1,5 +1,5 @@
 PORT=/dev/ttyACM0
-CODE=/testing/arduino_xbox/
+CODE=/arduino/i2c_demo
 
 echo "Compiling and uploading to ${PORT}..."
 arduino-cli compile --fqbn arduino:avr:uno ${CODE} --upload -p ${PORT} && echo "Done!"
