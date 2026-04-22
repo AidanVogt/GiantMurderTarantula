e5d5a77e7d101de234e5297979e8379d013d8eb9
AidanVogt <avogt671098@gmail.com>
2026-04-22 13:04:07 -0400

    working arduino for now - contact sensor commented out

--- leg_controller/leg_controller.ino
+++ leg_controller/leg_controller.ino
@@ -3,16 +3,16 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
-// I2C LEG ADDRESS (use 0x10 to 0x15)
-#define INO_ADDRESS 0x11
+#define INO_ADDRESS 0x15
 
-
-#define IS_LEFT_STEPPER (INO_ADDRESS == 0x12 || INO_ADDRESS == 0x13) ? 1 : 0
-#define REVERSE_DIRECTION INO_ADDRESS >= 0x13 ? 1 : 0
+#define IS_LEFT_STEPPER ((INO_ADDRESS == 0x12 || INO_ADDRESS == 0x13) ? 1 : 0)
+#define REVERSE_DIRECTION ((INO_ADDRESS >= 0x13) ? 1 : 0)
+#define STEP_UP_MULTIPLIER ((INO_ADDRESS == 0x13) ? 2 : 1)
+#define STEP_UP_ITERS 800
 
 #define MAX485_DE      8
 #define MAX485_RE_NEG  9
-#define RX_RS485 10 
+#define RX_RS485 10
 #define TX_RS485 11
 
 #define HALL_A 2
@@ -29,9 +29,11 @@
 #define MOTOR_SPEED 175
 #define HIP_MOVE_INTERVAL 20
 #define HIP_HOME_INTERVAL 5
+#define STEP_MAX_TIME_CONSTANT 50
+#define MAX_ANGLE 25
 
 #define PRINT_INTERVAL 500
-#define STEP_MAX_TIME_CONSTANT 50
+#define HOMING_THRESHOLD 1
 
 #define NOT_DONE 0
 #define DONE 1
@@ -40,11 +42,12 @@
 #define ACTION_FORWARD 1
 #define ACTION_BACKWARD 2
 #define ACTION_UP 3
-#define ACTION_DOWN 4
+#define ACTION_DOWN 4 
 #define ACTION_HOME_FORWARD 5
 #define ACTION_HOME_BACKWARD 6
+#define ACTION_ZERO 7
+#define ACTION_HOME 8
 
-// use software serial for uno boards, serial1 for minima
 #if INO_ADDRESS == 0x10 || INO_ADDRESS == 0x11
   SoftwareSerial RS485_serial(RX_RS485, TX_RS485);
 #elif INO_ADDRESS >= 0x12 && INO_ADDRESS <= 0x15
@@ -59,8 +62,9 @@ ModbusMaster node;
 bool forward = true;
 unsigned long last_print = 0;
 unsigned char current_action = ACTION_NONE;
-
-//////////////// MOTOR MOVEMENT FUNCS ////////////////
+float zero_offset = 0;
+float min_angle = -MAX_ANGLE;
+float max_angle = MAX_ANGLE;
 
 void preTransmission()
 {
@@ -87,6 +91,10 @@ void setMotorState(bool EN, bool FR, bool BK) {
   node.writeSingleRegister(0x8000, ((uint16_t)high << 8) | 0x5);
 }
 
+float get_angle() {
+  return encoder.getAngle() - zero_offset;
+}
+
 void set_motor_speed() {
   uint8_t result = node.writeSingleRegister(0x8005, MOTOR_SPEED);
   Serial.print("set motor speed result: ");
@@ -101,7 +109,6 @@ bool is_hitting_limit() {
   return !digitalRead(LIMIT_PIN);
 }
 
-// not recommended for normal operation
 void braking_stop() {
   Serial.println("braking stop (NOT RECOMMENDED)");
   setMotorState(0,0,1);
@@ -125,19 +132,25 @@ void set_backward() {
 void step_down() {
   Serial.println("stepping down");
   digitalWrite(DIR_PIN, IS_LEFT_STEPPER);
-  for (int i = 0; i < 300; i++) {
+  digitalWrite(EN_PIN, LOW);
+  for (int i = 0; i < (STEP_UP_ITERS * 0.8); i++) {
+    // if (is_contacting_ground())
+    //   break;
     encoder.update();
     digitalWrite(PUL_PIN, LOW);
     delayMicroseconds(1000);
     digitalWrite(PUL_PIN, HIGH);
     delayMicroseconds(1000);
   }
+  digitalWrite(EN_PIN, HIGH);
 }
 
 void step_up() {
-  Serial.println("stepping up");
+  Serial.print("stepping up by: ");
+  Serial.println(STEP_UP_ITERS);
   digitalWrite(DIR_PIN, !IS_LEFT_STEPPER);
-  for (int i = 0; i < 350; i++) {
+  digitalWrite(EN_PIN, LOW);
+  for (int i = 0; i < STEP_UP_ITERS; i++) {
     encoder.update();
     digitalWrite(PUL_PIN, LOW);
     delayMicroseconds(1000);
@@ -147,45 +160,67 @@ void step_up() {
 }
 
 void move_forward(float angle) {
-  float end_condition = encoder.getAngle() + angle;
+  float current_angle = get_angle();
+  if (current_angle >= max_angle) {
+    Serial.println("out of bounds");
+    return;
+  }
+
+  float end_condition = current_angle + angle;
+  if (end_condition > max_angle) end_condition = max_angle;
+
   Serial.print("end condition: ");
   Serial.println(end_condition);
   Serial.print("current angle: ");
-  Serial.println(encoder.getAngle());
+  Serial.println(current_angle);
+
   set_forward();
+
   unsigned long step_time = millis();
-  unsigned long max_time = angle * STEP_MAX_TIME_CONSTANT;
-  while (current_action != ACTION_NONE && encoder.getAngle() < end_condition) {
+  unsigned long max_time = (unsigned long)(angle * STEP_MAX_TIME_CONSTANT);
+
+  while (current_action != ACTION_NONE && get_angle() < end_condition && get_angle() < max_angle) {
     encoder.update();
 
     if (millis() - step_time > max_time) {
       Serial.print("step timeout breaking...");
       Serial.println(millis() - step_time);
       break;
-    } 
+    }
 
-    
     if (millis() - last_print > PRINT_INTERVAL) {
       Serial.print("end condition: ");
       Serial.print(end_condition);
       Serial.print(" current angle: ");
-      Serial.println(encoder.getAngle());
+      Serial.println(get_angle());
       last_print = millis();
     }
-  } 
+  }
+
   non_braking_stop();
 }
 
 void move_backward(float angle) {
-  float end_condition = encoder.getAngle() - angle;
+  float current_angle = get_angle();
+  if (current_angle <= min_angle) {
+    Serial.println("out of bounds");
+    return;
+  }
+
+  float end_condition = current_angle - angle;
+  if (end_condition < min_angle) end_condition = min_angle;
+
   Serial.print("end condition: ");
   Serial.println(end_condition);
   Serial.print("current angle: ");
-  Serial.println(encoder.getAngle());
+  Serial.println(current_angle);
+
   set_backward();
+
   unsigned long step_time = millis();
-  unsigned long max_time = angle * STEP_MAX_TIME_CONSTANT;
-  while (current_action != ACTION_NONE && encoder.getAngle() > end_condition) {
+  unsigned long max_time = (unsigned long)(angle * STEP_MAX_TIME_CONSTANT);
+
+  while (current_action != ACTION_NONE && get_angle() > end_condition && get_angle() > min_angle) {
     encoder.update();
 
     if (millis() - step_time > max_time) {
@@ -197,10 +232,11 @@ void move_backward(float angle) {
       Serial.print("end condition: ");
       Serial.print(end_condition);
       Serial.print(" current angle: ");
-      Serial.println(encoder.getAngle());
+      Serial.println(get_angle());
       last_print = millis();
     }
   }
+
   non_braking_stop();
 }
 
@@ -226,13 +262,30 @@ void handle_backward(int angle) {
     move_backward(angle);
 }
 
-////////////////// I2C PARSING ////////////////
+void zero_encoder() {
+  zero_offset = encoder.getAngle();
+  Serial.print("zeroed encoder: ");
+  Serial.println(zero_offset);
+  Serial.print("current_angle");
+  Serial.println(get_angle());
+}
+
+void go_home() {
+  float angle = get_angle();
+  if (abs(angle) < HOMING_THRESHOLD)
+    return;
+
+move_up();
+  if (angle < 0)
+    move_forward(-angle);
+  else if (angle >= 0)
+    move_backward(angle);
+  move_down();
+}
+
 void receiveCommand(int numBytes) {
   while (Wire.available()) {
-    encoder.update();
     unsigned char byte = Wire.read();
-    Serial.print("receibed");
-    Serial.println(byte);
     if (byte >= 0x10) continue;
     current_action = byte;
   }
@@ -242,10 +295,8 @@ void sendStatus() {
   Wire.write(current_action == ACTION_NONE);
 }
 
-////////////////// SETUP AND LOOP //////////////////
 void setup()
 {
-  // I2C
   Wire.begin(INO_ADDRESS);
   Wire.onReceive(receiveCommand);
   Wire.onRequest(sendStatus);
@@ -265,12 +316,11 @@ void setup()
 
   pinMode(EN_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
-  pinMode(PUL_PIN, OUTPUT); 
+  pinMode(PUL_PIN, OUTPUT);
 
-  digitalWrite(EN_PIN, LOW);
+  digitalWrite(EN_PIN, HIGH);
   digitalWrite(DIR_PIN, LOW);
   digitalWrite(PUL_PIN, LOW);
-
 
   RS485_serial.begin(9600);
   Serial.begin(115200);
@@ -300,10 +350,14 @@ void get_action_serial() {
       current_action = ACTION_DOWN;
     if (c == 'c')
       set_motor_speed();
-    if (c == 'h')
+    if (c == 'l')
       current_action = ACTION_HOME_FORWARD;
-    if (c == 'g')
+    if (c == 'k')
       current_action = ACTION_HOME_BACKWARD;
+    if (c == 'z')
+      current_action = ACTION_ZERO;
+    if (c == 'h')
+      current_action = ACTION_HOME;
   }
 }
 
@@ -312,17 +366,29 @@ void loop()
   encoder.update();
   get_action_serial();
   if        (current_action == ACTION_FORWARD) {
+    Serial.println("RECEIVED ACTION_FORWARD");
     handle_forward(HIP_MOVE_INTERVAL);
   } else if (current_action == ACTION_BACKWARD) {
+    Serial.println("RECEIVED ACTION_BACKWARD");
     handle_backward(HIP_MOVE_INTERVAL);
   } else if (current_action == ACTION_UP) {
+    Serial.println("RECEIVED ACTION_UP");
     move_up();
   } else if (current_action == ACTION_DOWN) {
+    Serial.println("RECEIVED ACTION_DOWN");
     move_down();
   } else if (current_action == ACTION_HOME_FORWARD) {
+    Serial.println("RECEIVED ACTION_HOME_FORWARD");
     handle_forward(HIP_HOME_INTERVAL);
   } else if (current_action == ACTION_HOME_BACKWARD) {
+    Serial.println("RECEIVED ACTION_HOME_BACKWARD");
     handle_backward(HIP_HOME_INTERVAL);
+  } else if (current_action == ACTION_ZERO) {
+    Serial.println("RECEIVED ACTION_ZERO");
+    zero_encoder();
+  } else if (current_action == ACTION_HOME) {
+    Serial.println("RECEIVED ACTION_HOME");
+    go_home();
   }
   current_action = ACTION_NONE;
 }
