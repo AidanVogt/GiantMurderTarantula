47c4cc6094b3c7fa1c7312972aba474e26546362
Peneeta Wojcik <94078632+peneeta@users.noreply.github.com>
2026-04-21 21:04:36 -0400

    add longer delay

--- gait_and_homing.py
+++ gait_and_homing.py
@@ -28,7 +28,7 @@ def CompleteOneMovementCycle(gait_type, bus: I2CBus):
     for inst in gait_type:
         MoveLegs(bus, inst)
         print("Done with current instruction")
-        time.sleep(1)
+        time.sleep(1.5)
        
 def StopHoming(bus, curr_leg, joystick):
     """
